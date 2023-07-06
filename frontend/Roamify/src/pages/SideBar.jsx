99b3e32c889e612e96fefd1735ab387977a85ec7
James Bellew <11jamesbellew@gmail.com>
2023-07-07 00:46:59 +0100

    test

--- frontend/Roamify/src/pages/SideBar.jsx
+++ frontend/Roamify/src/pages/SideBar.jsx
@@ -3,6 +3,8 @@ import {FaLocationArrow} from 'react-icons/fa';
 import {FaHome} from 'react-icons/fa';
 import {FaChartBar} from 'react-icons/fa';
 import {FaUser} from 'react-icons/fa';
+import {FaMoon} from 'react-icons/fa';
+import {FaRegCircle} from 'react-icons/fa';
 const SideBar = () => {
     
 function reset(){
@@ -51,19 +53,44 @@ function reset(){
 <span class="ml-3 hidden sm:block  text-text-secondary font-semibold tracking-wide hover:text-white transition-colors text-sm"> Profile</span>
   </li>
   
+<ul className='ml-6'>
+<li class="hover:bg-gray-800 cursor-pointer sm:justify-start px-4 h-12 flex items-center justify-center">
+
+
+<FaRegCircle className='h-2 w-2 text-purple-main shadow-purple-main shadow-2xl'/>
+
+
+<span class="ml-3 hidden sm:block   text-text-secondary font-semibold tracking-wide hover:text-white transition-colors text-sm"> Settings</span>
+
+</li>
+</ul>
+<ul className='ml-6'>
+<li class="hover:bg-gray-800 cursor-pointer sm:justify-start px-4 h-12 flex items-center justify-center">
+
+
+<FaRegCircle className='h-2 w-2 text-text-secondary shadow-purple-main shadow-2xl'/>
+
+
+<span class="ml-3 hidden sm:block   text-text-secondary font-semibold tracking-wide hover:text-white transition-colors text-sm"> Invite</span>
+
+</li>
+</ul>
 
 
  
 
 </ul>
 
-<div className='bg-background-main h-auto w-20 p-5  rounded
- mx-auto absolute bottom-10 hidden sm:block sm:w-40 sm:m-10'>
-<h1 className='text-center text-white'>Hello</h1>
-<br></br>
-<br></br>
-<button className="bg-purple-main/30 p-5 text-white  flex align-middle rounded-xl ">
-Get Started</button>
+<div className='bg-dark-grey h-auto w-20 p-5  rounded
+ mx-auto absolute bottom-10 hidden sm:block sm:w-52 sm:m-4'>
+  
+  <div className='bg-background-main w-12 h-12 flex mx-auto rounded-full'><FaUser className='text-white my-auto mx-auto '/></div>
+
+
+<p className='  text-base m-3 text-center mb-5 text-purple-main'>Guest</p> 
+
+<div className='bg-grey/20 w-14 h-8  mx-auto my-auto flex  rounded-full mt-10'><FaMoon className='my-auto ml-2 text-text-secondary'/></div>
+
  </div>
 
 </div>
