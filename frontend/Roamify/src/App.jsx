697a33d0f323ad9599ec211d2fb89dea4e7d3810
James_Bellew <11jamesbellew@gmail.com>
2023-07-07 13:56:32 +0100

    small change

--- frontend/Roamify/src/App.jsx
+++ frontend/Roamify/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
 
   return (
     <>
-     <main class="flex bg-background-main">
+     <main class="flex bg-background-main min-h-screen">
   <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />}>
