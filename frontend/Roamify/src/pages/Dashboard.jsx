e6d4daac55ae801349a5023e5ece88c199a49c18
James Bellew <11jamesbellew@gmail.com>
2023-07-15 01:43:07 +0100

    users saved countries now ssaved to db and when selected from form makes request to db

--- frontend/Roamify/src/pages/Dashboard.jsx
+++ frontend/Roamify/src/pages/Dashboard.jsx
@@ -19,10 +19,6 @@ import {
 // import { getFirestore, setDoc ,doc, updateDoc, addDoc,getDoc, QuerySnapshot} from 'firebase/firestore'
 
 const Dashboard = () => {
-  const [user, loading] = useAuthState(auth);
-
-  const [tableData, setData] = useState([]);
-
   const firebaseConfig = {
     apiKey: "AIzaSyDKL_4B3j2OmIKPppgT0xrLjIQGv2Ru4Jo",
     authDomain: "roamify-9731d.firebaseapp.com",
@@ -33,6 +29,57 @@ const Dashboard = () => {
     appId: "1:431369203090:web:d380c8bfb258a10640e54b",
     measurementId: "G-TB8BJ8CGGS",
   };
+  const countriesArray = [];
+  const [user, loading] = useAuthState(auth);
+  const [data, setData] = useState([]);
+  const [countryBtnShow, updateShowBtn] = useState(false);
+  const app = initializeApp(firebaseConfig);
+  const db = getDatabase();
+  const reference = ref(db, "users/0123/countries");
+  const userId = user.uid;
+  const countrriesRef = ref(db, "users/" + userId + "/countries");
+  // const [tableData, setData] = useState([]);
+
+  // this is just an exmaple array used for the FaList, idealy we would want this coming froma  json file but this will do as a proof of consept
+  const [countryList, updateCountryList] = useState([
+    "Finland",
+    "Ireland",
+    "Sweden",
+    "Spain",
+    "Austria",
+    "Latvia",
+    "Slovenia",
+    "England",
+    "Holland",
+    "France",
+    "Italy",
+    "Croatia",
+  ]);
+  function remove(arr, what) {
+    var found = arr.indexOf(what);
+    console.log(arr);
+    console.log(what);
+    console.log("called");
+    while (found !== -1) {
+      arr.splice(found, 1);
+      found = arr.indexOf(what);
+    }
+  }
+  // now we want to check has the user been to any of the example array countries
+  onValue(countrriesRef, (snapshot) => {
+    // Iterate over each child snapshot within the "countries" list
+    snapshot.forEach((childSnapshot) => {
+      // Get the data from the child snapshot and push it to the array
+      const countryData = childSnapshot.val();
+      countriesArray.push(countryData);
+    });
+  });
+  console.log("User Database countries", countriesArray);
+  for (let i = 0; i < countriesArray.length; i++) {
+    console.log(countriesArray[i]);
+    // countryList.filter((v) => v !== countriesArray[i]);
+    remove(countryList, countriesArray[i]);
+  }
 
   // This function is called on the button clicked, all information will be supplied byt he google auth object besides the countries array
   function testDataWrite(userId, name, email, imageUrl, countries) {
@@ -44,7 +91,10 @@ const Dashboard = () => {
       const countriesRef = ref(db, "users/" + userId + "/countries");
       countryArray.forEach((country) => {
         push(countriesRef, country);
+        updateShowBtn(false);
       });
+      // the below is neede to remove the selected from the temp rray since it way added to the visited array
+
       // the below commented out is not needed Headers, but will be needed in the signup part to store userts info
       // update(ref(db, "users/" + userId), {
       //   username: name,
@@ -53,10 +103,6 @@ const Dashboard = () => {
       // });
     }
   }
-
-  const app = initializeApp(firebaseConfig);
-  const db = getDatabase();
-  const reference = ref(db, "users/0123/countries");
 
   onValue(reference, (snapshot) => {
     const records = [];
@@ -69,9 +115,13 @@ const Dashboard = () => {
   });
   // the below array and function is for when the user clicks on a checkbox of a country it will be added to a temp array(useState array countryArray) awaiting for the user to click on the save button and then this will be added to the firabse databse
   const [countryArray, updateCountryArray] = useState([]);
+
   function handleOnChange(name) {
     updateCountryArray((countryArray) => [...countryArray, name]);
+    updateShowBtn(true);
+
     // the below code asks the user is they want to reload the page as their changes wont be saved unless they click on the submit button
+
     window.addEventListener("beforeunload", function (e) {
       var confirmationMessage =
         "It looks like you have been editing something. " +
@@ -81,7 +131,28 @@ const Dashboard = () => {
       return confirmationMessage; //Gecko + Webkit, Safari, Chrome etc.
     });
   }
-  console.log(countryArray);
+
+  useEffect(() => {
+    // Create a reference to the desired location in the database
+    const reference = ref(db, "users/" + userId + "/countries");
+
+    // Attach an event listener to listen for changes in the data
+    const unsubscribe = onValue(reference, (snapshot) => {
+      const dataFromDb = snapshot.val();
+      // Update the state with the retrieved data
+      setData(dataFromDb);
+      // console.log(data);
+      // console.log("above baiiiii");
+      // console.log(Object.values(data));
+      // tempArray = Object.values(data);
+    });
+
+    // Clean up the event listener when the component is unmounted
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
   //  This is the return JSX for this file
   return (
     <>
@@ -93,11 +164,30 @@ const Dashboard = () => {
           {user && (
             <>
               <div class="grid sm:grid-cols-2  xl:grid-cols-4  gap-4">
-                <div className="bg-background-main/10 rounded p-5 min-h-[20vh]">
+                <div className="bg-background-main/10 rounded p-5 min-h-[40vh]">
                   <h3 class="mb-4 font-semibold text-gray-900 dark:text-white">
                     Europe
                   </h3>
-                  <ul class="w-full mx-auto text-sm font-medium text-gray-900 bg-white   rounded-lg dark:bg-background-main/20   dark:text-white">
+                  <ul class="w-full mx-auto text-sm font-medium text-gray-900 bg-white   rounded-lg dark:bg-background-main/10 shadow-sm   dark:text-white">
+                    {countryList.map((country) => (
+                      // <li key={country.id}>{country}</li>
+                      <li class="w-full  border-gray-200 rounded-t-lg dark:border-gray-600">
+                        <div class="flex items-center pl-3">
+                          <input
+                            type="checkbox"
+                            onChange={() => handleOnChange(country)}
+                            class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+                          />
+                          <label
+                            for="vue-checkbox"
+                            class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+                            {country}
+                          </label>
+                        </div>
+                      </li>
+                    ))}
+                  </ul>
+                  {/* <ul class="w-full mx-auto text-sm font-medium text-gray-900 bg-white   rounded-lg dark:bg-background-main/20   dark:text-white">
                     <li class="w-full  border-gray-200 rounded-t-lg dark:border-gray-600">
                       <div class="flex items-center pl-3">
                         <input
@@ -158,31 +248,85 @@ const Dashboard = () => {
                         </label>
                       </div>
                     </li>
-                  </ul>
+                  </ul> */}
+                </div>
+                <div className="bg-background-main/20 shadow rounded p-3 min-h-[20vh]">
+                  <h1 className="text-white text-lg">Asia</h1>
+                  <div></div>
                 </div>
                 <div className="bg-background-main/10 rounded p-3 min-h-[20vh]">
-                  <h1 className="text-white text-lg">Europe</h1>
+                  <h1 className="text-white text-lg">North America</h1>
                 </div>
                 <div className="bg-background-main/10 rounded p-3 min-h-[20vh]">
-                  <h1 className="text-white text-lg">Europe</h1>
-                </div>
-                <div className="bg-background-main/10 rounded p-3 min-h-[20vh]">
-                  <h1 className="text-white text-lg">Europe</h1>
+                  <h1 className="text-white text-lg">Africa</h1>
                 </div>
               </div>
+              {countryBtnShow && (
+                <button
+                  onClick={() =>
+                    testDataWrite(
+                      user.uid,
+                      user.displayName,
+                      user.email,
+                      user.photoURL
+                    )
+                  }
+                  className="bg-purple-main p-4 rounded mx-auto text-center flex my-10">
+                  Add Data
+                </button>
+              )}
+              <div class="grid sm:grid-cols-1 over  xl:grid-cols-2 mt-5 gap-4">
+                <div className="bg-background-main/10 overflow-auto rounded p-3 h-[20vh]">
+                  <h1 className="text-white text-lg mb-5">Visited Countries</h1>
 
-              <button
-                onClick={() =>
-                  testDataWrite(
-                    user.uid,
-                    user.displayName,
-                    user.email,
-                    user.photoURL
-                  )
-                }
-                className="bg-pink-main p-4 rounded mx-auto text-center flex">
-                Add Data
-              </button>
+                  <div class="relative overflow-x-auto">
+                    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                      <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-background-main/20 dark:text-gray-400">
+                        <tr>
+                          <th scope="col" class="px-6 py-3">
+                            Product name
+                          </th>
+                          <th scope="col" class="px-6 py-3">
+                            Region
+                          </th>
+                          <th scope="col" class="px-6 py-3">
+                            Remove
+                          </th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {data &&
+                          Object.keys(data).map((key) => (
+                            // <div key={key}>{data[key]}</div>
+                            <tr
+                              key={key}
+                              class="bg-white border-b dark:bg-background-main/40 dark:border-gray-700">
+                              <th
+                                scope="row"
+                                class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                {data[key]}
+                              </th>
+                              <td class="px-6 py-4">Europe</td>
+                              <td class="px-6 py-4 mx-auto">
+                                <input
+                                  type="checkbox"
+                                  onChange={() =>
+                                    remove(countriesArray, data[key])
+                                  }
+                                  class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+                                />
+                              </td>
+                            </tr>
+                          ))}
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+                <div className="bg-background-main/10 rounded p-3 min-h-[20vh]">
+                  <h1 className="text-white text-lg">Statistics</h1>
+                  <div></div>
+                </div>
+              </div>
             </>
           )}
           {Cookies.get("GuestLoginStatus") == "true" && (
--- frontend/Roamify/src/pages/Login.jsx
+++ frontend/Roamify/src/pages/Login.jsx
@@ -1,60 +1,75 @@
-import React, { useState } from 'react';
-import {FcGoogle} from "react-icons/fc";
-import {AiFillFacebook} from "react-icons/ai"
-import {FaUserAlt} from "react-icons/fa"
-import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
-import { auth } from '../utils/firebase';
+import React, { useState } from "react";
+import { FcGoogle } from "react-icons/fc";
+import { AiFillFacebook } from "react-icons/ai";
+import { FaUserAlt } from "react-icons/fa";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { auth } from "../utils/firebase";
 import { useNavigate } from "react-router-dom";
-import { useCookies } from 'react-cookie';
-import Cookies from 'js-cookie';
+import { useCookies } from "react-cookie";
+import Cookies from "js-cookie";
 const Login = () => {
-    const navigate = useNavigate();
-    const GoogleProvider = new GoogleAuthProvider();
-    const GoogleLogin = async ()=>{
-        try {
-            const result = await signInWithPopup(auth,GoogleProvider);
-            navigate("/dashboard");
-        } catch (error) {
-            console.log(result.user);
-            // navigate("/dashboard");
-
-        }
+  const navigate = useNavigate();
+  const GoogleProvider = new GoogleAuthProvider();
+  const GoogleLogin = async () => {
+    try {
+      const result = await signInWithPopup(auth, GoogleProvider);
+      navigate("/dashboard");
+    } catch (error) {
+      console.log(result.user);
+      // navigate("/dashboard");
     }
-    // const [guestStatus, setGuestStatus] = useState('not logged in');
+  };
+  // const [guestStatus, setGuestStatus] = useState('not logged in');
 
+  if (Cookies.get("GuestLoginStatus") == null) {
+    Cookies.set("GuestLoginStatus", false);
+  }
+  //   console.log(Cookies.get('GuestLoginStatus')+'boiiii');
+  const GuestLoginHandler = () => {
+    Cookies.set("GuestLoginStatus", true);
+    location.reload();
+    navigate("/dashboard");
+    console.log(Cookies.get("GuestLoginStatus"));
+  };
+  return (
+    <>
+      <div className="rounded-lg shadow-xl text-center  sm:p-20 sm:m-20 p-5 dark:bg-dark-grey  mx-auto text-text-secondary my-auto">
+        <h1 className="text-3xl font-medium text-center dark:text-white">
+          Welcome to Roamify
+        </h1>
 
-    if(Cookies.get('GuestLoginStatus') ==null){
-    Cookies.set('GuestLoginStatus', false);
-    }
-    //   console.log(Cookies.get('GuestLoginStatus')+'boiiii');
-    const GuestLoginHandler=()=>{
-        Cookies.set('GuestLoginStatus',true);
-        location.reload();
-        navigate("/dashboard");
-        console.log(Cookies.get('GuestLoginStatus'));
-    }
-    return (
-     <>
-     
-        <div className="rounded-lg shadow-xl text-center  sm:p-20 sm:m-20 p-5 dark:bg-dark-grey  mx-auto text-text-secondary my-auto">
-        <h1 className="text-3xl font-medium text-center dark:text-white">Welcome to Roamify</h1>
-   
-<div className="py-4">        <h3 className='text-center text-text-secondary'>Sign in with one of the below</h3>
-</div>
+        <div className="py-4">
+          {" "}
+          <h3 className="text-center text-text-secondary">
+            Sign in with one of the below
+          </h3>
+        </div>
 
- <div className="py-3 flex flex-col gap-3">
-    {/* <button  className="bg-purple-main/30 dark:text-white p-5 flex align-middle rounded-xl md:m-2 gap-5"><FcGoogle className="text-2xl  "/>  Sign in with Google</button> */}
-    <button onClick={GoogleLogin} className="bg-purple-main/30 text-white p-5 flex align-middle rounded-xl m-2 mt-4 gap-5 mx-auto"><FcGoogle className="text-2xl  "/>Sign in with Google</button>
-    <button  className="bg-purple-main/30 text-white p-5 flex align-middle rounded-xl m-2 mt-4 gap-5 mx-auto"><AiFillFacebook className="text-2xl text-facebook  "/>Sign in with Facebook</button>
-    {/* <button className="bg-purple-main/30 dark:text-white p-5 flex align-middle rounded-xl md:m-2 gap-5"><AiFillFacebook className="text-2xl text-facebook"/>Sign in with Facebook</button> */}
-</div> 
+        <div className="py-3 flex flex-col gap-3">
+          {/* <button  className="bg-purple-main/30 dark:text-white p-5 flex align-middle rounded-xl md:m-2 gap-5"><FcGoogle className="text-2xl  "/>  Sign in with Google</button> */}
+          <button
+            onClick={GoogleLogin}
+            className="bg-purple-main/30 text-white p-5 flex align-middle rounded-xl m-2 mt-4 gap-5 mx-auto">
+            <FcGoogle className="text-2xl  " />
+            Sign in with Google
+          </button>
+          <button className="bg-purple-main/30 text-white p-5 flex align-middle rounded-xl m-2 mt-4 gap-5 mx-auto">
+            <AiFillFacebook className="text-2xl text-facebook  " />
+            Sign in with Facebook
+          </button>
+          {/* <button className="bg-purple-main/30 dark:text-white p-5 flex align-middle rounded-xl md:m-2 gap-5"><AiFillFacebook className="text-2xl text-facebook"/>Sign in with Facebook</button> */}
+        </div>
 
-<hr className='m-10'/>
-{/* <h2 className="text-3xl font-medium mt-6 mx-auto text-center text-white">Continue as Guest</h2> */}
-<button onClick={GuestLoginHandler}  className="bg-pink-main text-white p-5 flex align-middle rounded-xl m-2 mt-4 gap-5 mx-auto"><FaUserAlt className='text-2xl'/> Continue as Guest</button>
-</div>
-     </>
-    );
+        <hr className="m-10" />
+        {/* <h2 className="text-3xl font-medium mt-6 mx-auto text-center text-white">Continue as Guest</h2> */}
+        <button
+          onClick={GuestLoginHandler}
+          className="bg-pink-main text-white p-5 flex align-middle rounded-xl m-2 mt-4 gap-5 mx-auto">
+          <FaUserAlt className="text-2xl" /> Continue as Guest
+        </button>
+      </div>
+    </>
+  );
 };
 
 export default Login;
